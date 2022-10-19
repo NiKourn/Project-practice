d226944c10c2819e916add78f2d253c66bca6468
NiKourn <nikourn@gmail.com>
2022-10-19 21:47:58 +0100

    Update app.php

--- app.php
+++ app.php
@@ -1,2 +1,10 @@
 <?php
 echo '<h1>App Page: Connection with db succesfull</h1>';
+
+echo "<div class=''>";
+$count = 1;
+foreach ( get_included_files() as $included_file ) {
+	echo $count . '. ' . $included_file . '<br>';
+	$count ++;
+}
+echo "</div>";
