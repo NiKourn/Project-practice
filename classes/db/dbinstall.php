16795b1ee7ef5fb64143814139c2f46b7c6d5544
NiKourn <nikourn@gmail.com>
2022-10-21 18:06:16 +0100

    nonce seems to be working with $_SESSION

--- classes/db/dbinstall.php
+++ classes/db/dbinstall.php
@@ -145,36 +145,26 @@ class dbinstall {
 	 * @return void
 	 */
 	private function build_db() {
-		if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' ) {
-			$token     = $_POST['token'] ;
+			$token     = $_SESSION['token'] ;
 			$testnonce = verifyNonce( $token );
-			echo '<pre>' . print_r($token, true) . '</pre>';
-			echo '<pre>' . print_r($_SESSION, true) . '</pre>';
-			if ( $testnonce){
-				echo 'true';
-			}else{
-				echo 'false';
-			}
 			
 			if ( $testnonce === false ) {
 				return;
 			}
 			
-			
-			if ( empty( $_POST[ 'token' ] ) ) {
+			if ( empty( $_SESSION[ 'token' ] ) || empty ($_POST['token']) ) {
 				return;
 			}
-			die();
 			unset($_SESSION, $token);
+			//add details to Json before creating something to store info
 		$json_details = $this->form_post_details( $_POST[ 'host' ], $_POST[ 'db_name' ], $_POST[ 'root_username' ], $_POST[ 'root_password' ], $_POST[ 'db_username' ], $_POST[ 'db_password' ] );
-		
+		die();
 			
 			if ( ! self::get_db() ) {
 				
 				$this->create_db->createDB( $json_details[ 'host' ], $json_details[ 'db_name' ], $json_details[ 'root_username' ], $json_details[ 'root_password' ], $json_details[ 'db_username' ], $json_details[ 'db_password' ] );
 				$this->create_db->createTables( $json_details[ 'host' ], $json_details[ 'db_name' ], $json_details[ 'db_username' ], $json_details[ 'db_password' ] );
 			}
-		}
 		
 	}
 	
